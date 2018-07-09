cf2396f65beb12c94f6ff589d22362bd729b8bb3
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-09 16:12:24 +0200

    Added Initial Logic And UI For MemeMe App

--- MemeMeApp/MemeMeApp/ViewController.swift
+++ MemeMeApp/MemeMeApp/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var pickButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    var imagePickerController: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        pickButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        showImagePickerController(sourceType: .camera)
+    }
+    
+    @IBAction func pickAnImageFromPhotoLibrary(_ sender: Any) {
+        showImagePickerController(sourceType: .photoLibrary)
+    }
+    
+    func showImagePickerController(sourceType: UIImagePickerControllerSourceType) {
+        if sourceType == .camera {
+            imagePickerController.cameraDevice = .rear
+        }
+        imagePickerController.sourceType = sourceType
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image: UIImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
